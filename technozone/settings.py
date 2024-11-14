165c6508245858989715943e1196820e8023c48d
RumelNHORS <rumelnhors52@gmail.com>
2024-11-14 01:08:17 -0500

    add requirements.txt file and use the database as mysql.

--- technozone/settings.py
+++ technozone/settings.py
@@ -78,10 +78,23 @@ WSGI_APPLICATION = 'technozone.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+# technozone
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'technozone',
+        'USER': 'root',
+        'PASSWORD': 'new_password',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
