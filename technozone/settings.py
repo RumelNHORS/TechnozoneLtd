523010a44974c3572c80fbee61767efe7bd74d51
RumelNHORS <rumelnhors52@gmail.com>
2024-11-14 02:35:07 -0500

    use the jazzmin for the django admin dashboard.

--- technozone/settings.py
+++ technozone/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -42,6 +43,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'api',
     'ckeditor',
+    'ckeditor_uploader',
 ]
 
 MIDDLEWARE = [
@@ -134,6 +136,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 
 MEDIA_URL = '/media/'
@@ -167,4 +171,125 @@ CKEDITOR_CONFIGS = {
     },
 }
 
+
+# Jazzmin Configuration
+JAZZMIN_SETTINGS = {
+    "site_title": "Admin Panel | TechnoZoneLtd.",
+    "site_name": "TechnoZoneLtd.",
+    "site_header": "TechnoZoneLtd.",
+    "site_brand": "TechnoZoneLtd.",
+    "site_logo": "images/technozone.jpg",
+    "welcome_sign": "Welcome to TechnoZoneLtd.",
+    "site_icon": "images/technozone.jpg",
+    "topmenu_links": [
+        {"name": "Home", "url": "admin:index", "permissions": ["auth.view_user"]},
+        {"name": "Users", "url": "/admin/userauths/user/"},
+        {"model": "AUTH_USER_MODEL.User"},
+    ],
+
+    "icons": {
+        "admin.LogEntry": "fas fa-file",
+        "auth": "fas fa-users-cog",
+        "auth.user": "fas fa-user",
+        "userauths.User": "fas fa-user",
+        "userauths.Profile": "fas fa-address-card",
+        "api.AboutUs": "fas fa-info-circle",
+        "api.Service": "fas fa-hand-holding-heart",
+        "api.Project": "fas fa-project-diagram",
+        "hotel.Guest": "fas fa-user",
+        "hotel.Room": "fas fa-bed",
+        "hotel.hotelfaqs": "fas fa-question-circle",
+        "hotel.hotelfeatures": "fas fa-info-circle",
+        "hotel.hotelgallery": "fas fa-image",
+        "hotel.roomtype": "fas fa-bed",
+        "hotel.RoomServices": "fas fa-user-cog",
+        "hotel.Notification": "fas fa-bell",
+        "hotel.Coupon": "fas fa-tag",
+        "hotel.Bookmark": "fas fa-heart",
+    },
+    "show_ui_builder": True,
+    "default_icon_parents": "fas fa-chevron-circle-right",
+    "default_icon_children": "fas fa-arrow-circle-right",
+}
+
+JAZZMIN_UI_TWEAKS = {
+    "navbar_small_text": False,
+    "footer_small_text": False,
+    "body_small_text": False,
+    "brand_small_text": False,
+    "brand_colour": "navbar-warning",
+    "accent": "accent-orange",
+    "navbar": "navbar-dark",
+    "no_navbar_border": False,
+    "navbar_fixed": False,
+    "layout_boxed": False,
+    "footer_fixed": False,
+    "sidebar_fixed": False,
+    "sidebar": "sidebar-dark-info",
+    "sidebar_nav_small_text": False,
+    "sidebar_disable_expand": False,
+    "sidebar_nav_child_indent": False,
+    "sidebar_nav_compact_style": False,
+    "sidebar_nav_legacy_style": False,
+    "sidebar_nav_flat_style": False,
+    "theme": "default",
+    "dark_mode_theme": None,
+    "button_classes": {
+        "primary": "btn-primary",
+        "secondary": "btn-secondary",
+        "info": "btn-info",
+        "warning": "btn-warning",
+        "danger": "btn-danger",
+        "success": "btn-warning",
+    },
+}
+
+# CKEditor5 Configuration
+customColorPalette = [
+    {'color': 'hsl(24, 90%, 58%)', 'label': 'Orange'},
+    {'color': 'hsl(36, 77%, 49%)', 'label': 'Amber'},
+    {'color': 'hsl(14, 82%, 52%)', 'label': 'Dark Orange'},
+    {'color': 'hsl(46, 89%, 55%)', 'label': 'Gold'},
+    {'color': 'hsl(16, 100%, 60%)', 'label': 'Bright Orange'},
+    {'color': 'hsl(30, 70%, 50%)', 'label': 'Burnt Orange'},
+]
+
+CKEDITOR_5_CONFIGS = {
+    'default': {
+        'toolbar': ['heading', '|', 'bold', 'italic', 'link', 'bulletedList', 'numberedList', 'blockQuote', 'imageUpload'],
+    },
+    'extends': {
+        'blockToolbar': ['paragraph', 'heading1', 'heading2', 'heading3', '|', 'bulletedList', 'numberedList', '|', 'blockQuote'],
+        'toolbar': [
+            'heading', '|', 'outdent', 'indent', '|', 'bold', 'italic', 'link', 'underline', 'strikethrough', 'code',
+            'subscript', 'superscript', 'highlight', '|', 'codeBlock', 'sourceEditing', 'insertImage', 'bulletedList',
+            'numberedList', 'todoList', '|', 'blockQuote', 'imageUpload', '|', 'fontSize', 'fontFamily', 'fontColor',
+            'fontBackgroundColor', 'mediaEmbed', 'removeFormat', 'insertTable',
+        ],
+        'image': {
+            'toolbar': ['imageTextAlternative', '|', 'imageStyle:alignLeft', 'imageStyle:alignRight', 'imageStyle:alignCenter', 'imageStyle:side', '|'],
+            'styles': ['full', 'side', 'alignLeft', 'alignRight', 'alignCenter'],
+        },
+        'table': {
+            'contentToolbar': ['tableColumn', 'tableRow', 'mergeTableCells', 'tableProperties', 'tableCellProperties'],
+            'tableProperties': {'borderColors': customColorPalette, 'backgroundColors': customColorPalette},
+            'tableCellProperties': {'borderColors': customColorPalette, 'backgroundColors': customColorPalette},
+        },
+        'heading': {
+            'options': [
+                {'model': 'paragraph', 'title': 'Paragraph', 'class': 'ck-heading_paragraph'},
+                {'model': 'heading1', 'view': 'h1', 'title': 'Heading 1', 'class': 'ck-heading_heading1'},
+                {'model': 'heading2', 'view': 'h2', 'title': 'Heading 2', 'class': 'ck-heading_heading2'},
+                {'model': 'heading3', 'view': 'h3', 'title': 'Heading 3', 'class': 'ck-heading_heading3'},
+            ],
+        },
+    },
+    'list': {
+        'properties': {
+            'styles': 'true',
+            'startIndex': 'true',
+            'reversed': 'true',
+        },
+    },
+}
 
